c9df0e2b5f9d78487a7eb73fabaccaceebb97879
vo ngoc nghia <vongocnghia1610@gmail.com>
2021-07-05 23:06:27 +0700

    [Nghia:controller] add edit and show done task

--- src/app/controllers/TaskControllers.js
+++ src/app/controllers/TaskControllers.js
@@ -29,6 +29,20 @@ class taskController {
       });
     }
   }
+  async showDoneTask(req, res, next) {
+    try {
+      var result = await todos.find({ status: "DONE" });
+      res.status(200).send({
+        data: result,
+        error: "null",
+      });
+    } catch (error) {
+      res.status(500).send({
+        data: "",
+        error: error,
+      });
+    }
+  }
   async deleteTask(req, res, next) {
     try {
       var _id = req.body.idTask;
@@ -54,6 +68,27 @@ class taskController {
       var result = await todos.findOneAndUpdate(
         _id,
         { status: "DONE" },
+        { new: true }
+      );
+      res.status(200).send({
+        data: result,
+        error: "null",
+      });
+    } catch (error) {
+      res.status(500).send({
+        data: "",
+        error: error,
+      });
+    }
+  }
+  async editTask(req, res, next) {
+    try {
+      var _id = req.params.id;
+      var update = { title: req.body.title, subTitle: req.body.subTitle };
+
+      var result = await todos.findOneAndUpdate(
+        _id,
+        update,
         { new: true }
       );
       res.status(200).send({
--- src/routes/task.js
+++ src/routes/task.js
@@ -4,6 +4,8 @@ const taskController = require("../app/controllers/TaskControllers");
 router.post("/create-task", taskController.createTask);
 router.put("/delete-task", taskController.deleteTask);
 router.put("/done-task", taskController.doneTask);
+router.put("/edit-task", taskController.editTask);
 router.get("/show-task", taskController.showTask);
+router.get("/show-done-task", taskController.showDoneTask);
 
 module.exports = router;
